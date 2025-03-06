f732693e25b6a6416a2c2ea5edc5c522a254bae0
Kevin Rieger <kevin.rieger@sap.com>
2025-03-06 17:39:04 +0100

    Add basic layout for Svelte + Shadcn

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import LightSwitch from '$lib/components/lightswitch/LightSwitch.svelte';
+	import '../app.css';
+	import { ModeWatcher } from 'mode-watcher';
+
+	let { children } = $props();
+</script>
+
+<ModeWatcher />
+<LightSwitch />
+
+{@render children()}
